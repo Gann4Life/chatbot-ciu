2392d7236c4bc36fb0abfaafead96b01ef288e94
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2023-09-17 22:43:44 -0300

    Use mid quality bot on main branch

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -3,8 +3,8 @@ export const getChatbotAnswerFromPrompt = async (prompt) => {
 	try {
 		// USE ONLY ONE OF THE FOLLOWING FUNCTIONS
 
-		return await lowQualityResponse(prompt);
-		// return await midQualityResponse(prompt);
+		// return await lowQualityResponse(prompt);
+		return await midQualityResponse(prompt);
 		// return await highQualityResponse(prompt);
 	} catch (error) {
 		console.log(error);
