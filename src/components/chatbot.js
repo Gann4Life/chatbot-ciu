d7be57d49937e4f3106b300f3b09cd133bf9bae1
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2023-09-17 22:42:28 -0300

    Encapsulate bot functionality

---
+++ src/components/chatbot.js
@@ -0,0 +1,78 @@
+export const getChatbotAnswerFromPrompt = async (prompt) => {
+
+	try {
+		// USE ONLY ONE OF THE FOLLOWING FUNCTIONS
+
+		return await lowQualityResponse(prompt);
+		// return await midQualityResponse(prompt);
+		// return await highQualityResponse(prompt);
+	} catch (error) {
+		console.log(error);
+		return "Lo siento, no puedo responderte ahora mismo.";
+	}
+}
+
+// Low quality response, meaant to be used in the dev branch for testing out messages.
+async function lowQualityResponse(message) {
+	// Using robomatic.ai
+
+	const encodedParams = new URLSearchParams();
+	encodedParams.set('in', message);
+	encodedParams.set('op', 'in');
+	encodedParams.set('cbot', '1');
+	encodedParams.set('SessionID', 'RapidAPI1');
+	encodedParams.set('cbid', '1');
+	encodedParams.set('key', 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP');
+	encodedParams.set('ChatSource', 'RapidAPI');
+	encodedParams.set('duration', '1');
+
+	const url = 'https://robomatic-ai.p.rapidapi.com/api';
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/x-www-form-urlencoded',
+			'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+			'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com'
+		},
+		body: encodedParams
+	};
+
+	const response = await fetch(url, options);
+	const result = await response.text();
+	const resultJSON = JSON.parse(result);
+	return resultJSON.out;
+}
+
+// Mid quality response, meant to be used in the main branch for testing purposes.
+async function midQualityResponse(message){
+	const url = "https://ai-chatbot.p.rapidapi.com/chat/free?message=" + message.message + "&uid=user1";
+	const options = {
+	  method: 'GET',
+	  headers: {
+	    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+	    'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
+	  }
+	};
+
+	const response = await fetch(url, options);
+	const result = await response.text();
+	const resultJSON = JSON.parse(result)
+
+	return  resultJSON.chatbot.response;
+}
+
+// High quality response, meant to be used in the release branch.
+async function highQualityResponse(message){
+	const url = "https://chat-gpt-ai-bot.p.rapidapi.com/GenerateAIWritter?prompt=" + message.message;
+	const options = {
+	  method: 'GET',
+	  headers: {
+	    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+	    'X-RapidAPI-Host': 'chat-gpt-ai-bot.p.rapidapi.com'
+	  }
+	};
+
+	const response = await fetch(url, options);
+	const result = await response.text();
+	return result;
+}
--- src/components/messaging/chatPanel.js
+++ src/components/messaging/chatPanel.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import ChatArea from "./chatArea";
 import MessageInputArea from "./messageInputArea";
+import { getChatbotAnswerFromPrompt } from "../chatbot"
 
 const ChatPanel = () => {
 
@@ -9,10 +10,10 @@ const ChatPanel = () => {
 	const botProfileURL = "https://dthezntil550i.cloudfront.net/35/0023927117/e62cb797-db81-4f82-8320-388f910b38ec.jpg"
 	const userProfileURL = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
 
-	function sendMessageAsUser(newMessage) {
+	async function sendMessageAsUser(newMessage) {
 		newMessage.profileURL = userProfileURL;
 		logNewMessage(newMessage);
-		getThirdBotResponse(newMessage);
+		sendMessageAsBot(await getChatbotAnswerFromPrompt(newMessage));
 	}
 
 	function sendMessageAsBot(newMessage) {
@@ -28,87 +29,6 @@ const ChatPanel = () => {
 		setMessages(messages => [...messages, newMessage])
 	}
 
-
-	// Mid quality chatbot
-	async function getFirstBotResponse(message){
-		const url = "https://ai-chatbot.p.rapidapi.com/chat/free?message=" + message.message + "&uid=user1";
-		const options = {
-		  method: 'GET',
-		  headers: {
-		    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-		    'X-RapidAPI-Host': 'ai-chatbot.p.rapidapi.com'
-		  }
-		};
-
-		try {
-			const response = await fetch(url, options);
-			const result = await response.text();
-			const resultJSON = JSON.parse(result)
-
-			sendMessageAsBot(resultJSON.chatbot.response)
-		} catch (error) {
-			console.error(error);
-			sendMessageAsBot("Lo siento, no puedo responderte ahora mismo.");
-		}
-	}
-
-	// GOOD QUALITY CHATBOT
-	async function getSecondBotResponse(message){
-		const url = "https://chat-gpt-ai-bot.p.rapidapi.com/GenerateAIWritter?prompt=" + message.message;
-		const options = {
-		  method: 'GET',
-		  headers: {
-		    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-		    'X-RapidAPI-Host': 'chat-gpt-ai-bot.p.rapidapi.com'
-		  }
-		};
-
-		try {
-			const response = await fetch(url, options);
-			const result = await response.text();
-			sendMessageAsBot(result)
-		} catch (error) {
-			console.error(error);
-			sendMessageAsBot("Lo siento, no puedo responderte ahora mismo.");
-		}
-	}
-
-	// Low quality chatbot
-	async function getThirdBotResponse(message) {
-		// Using robomatic.ai
-
-		const encodedParams = new URLSearchParams();
-		encodedParams.set('in', message);
-		encodedParams.set('op', 'in');
-		encodedParams.set('cbot', '1');
-		encodedParams.set('SessionID', 'RapidAPI1');
-		encodedParams.set('cbid', '1');
-		encodedParams.set('key', 'RHMN5hnQ4wTYZBGCF3dfxzypt68rVP');
-		encodedParams.set('ChatSource', 'RapidAPI');
-		encodedParams.set('duration', '1');
-
-		const url = 'https://robomatic-ai.p.rapidapi.com/api';
-		const options = {
-		  method: 'POST',
-		  headers: {
-		    'content-type': 'application/x-www-form-urlencoded',
-		    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-		    'X-RapidAPI-Host': 'robomatic-ai.p.rapidapi.com'
-		  },
-		  body: encodedParams
-		};
-
-		try {
-			const response = await fetch(url, options);
-			const result = await response.text();
-			const resultJSON = JSON.parse(result);
-			sendMessageAsBot(resultJSON.out)
-		} catch (error) {
-			console.error(error);
-			sendMessageAsBot("Lo siento, no puedo responderte ahora mismo.");
-		}
-	}
-
 	return (
 		<div className="chat-panel">
 			<ChatArea
@@ -117,6 +37,7 @@ const ChatPanel = () => {
 			<MessageInputArea
 				addNewMessageFunction={sendMessageAsUser}
 			/>
+
 		</div>
 	)
 }
